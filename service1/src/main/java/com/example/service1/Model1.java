8a849ede13ca0d163f0b1268ed746864a4ae02ea
brascat <catarina.bras@diconium.com>
2023-11-21 11:31:21 +0000

    add getters and setters

--- service1/src/main/java/com/example/service1/Model1.java
+++ service1/src/main/java/com/example/service1/Model1.java
@@ -4,4 +4,12 @@ public class Model1 {
     private static final String HELLO = "Hello";
     private static final String WORLD = "World";
     private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
