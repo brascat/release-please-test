a1bf9a552db6ac0b39b6ec28d56a45e982408834
brascat <catarina.bras@diconium.com>
2023-12-05 13:28:37 +0000

    feat: add field to service1

--- service1/src/main/java/com/example/service1/Model1.java
+++ service1/src/main/java/com/example/service1/Model1.java
@@ -5,6 +5,7 @@ public class Model1 {
     private static final String WORLD = "World";
     private int count = 0;
     private int test0;
+    private int test1;
 
     public int getCount() {
         return count;
