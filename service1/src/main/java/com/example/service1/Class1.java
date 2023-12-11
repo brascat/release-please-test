73e363e983841263ccbfdc4e331292f73b133bb4
brascat <catarina.bras@diconium.com>
2023-12-11 11:58:37 +0000

    feat(test): affects service 1 and 2

--- service1/src/main/java/com/example/service1/Class1.java
+++ service1/src/main/java/com/example/service1/Class1.java
@@ -4,6 +4,7 @@ public class Class1 {
 
     private String test = "test";
     private String test2 = "test 2";
+    private String test3 = "test 3";
 
     public Class1() {
     }
--- service2/src/main/java/com/example/service2/Service2.java
+++ service2/src/main/java/com/example/service2/Service2.java
@@ -5,6 +5,7 @@ public class Service2 {
     private String hello;
     private int test0;
     private int test1;
+    private int test2;
 
     public Service2(String hello) {
         this.hello = hello;
