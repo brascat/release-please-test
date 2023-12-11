a7be612681822925cbfc2b2be89a71103d7536d2
brascat <catarina.bras@diconium.com>
2023-12-11 10:35:40 +0000

    feat: add configs

--- release.config.js
+++ release.config.js
@@ -1,4 +1,14 @@
+let service = process.env.SERVICE;
+
 module.exports = {
-    branch: 'main',
-    commitPaths: [`${process.env.SERVICE}/*`]
+    branches: 'main',
+    commitPaths: [`${service}/*`, 'common/*'],
+    tagFormat: service + '-v${version}',
+    repositoryUrl:'https://github.com/brascat/release-please-test',
+    debug:'false',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+    ]
 }
