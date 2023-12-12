980fbb489dcb28affd87eb4e2f73338fd23ef87c
brascat <catarina.bras@diconium.com>
2023-12-12 11:46:15 +0000

    feat!: test

--- release.config.js
+++ release.config.js
@@ -20,5 +20,18 @@ module.exports = {
                 discussionCategoryName: 'announcements'
             }
         ]
-    ]
+    ],
+    preset: 'conventionalcommits',
+    presetConfig: {
+        types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', section: 'Chores' },
+            { type: 'docs', hidden: true },
+            { type: 'style', hidden: true },
+            { type: 'refactor', section: 'Refactoring' },
+            { type: 'perf', hidden: true },
+            { type: 'test', hidden: true },
+        ],
+    }
 }
