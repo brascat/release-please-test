a7ee9474235d09a5888ade8eb1aadfe3147da22c
brascat <catarina.bras@diconium.com>
2023-12-12 17:08:27 +0000

    feat: add test6

--- service1/src/main/java/com/example/service1/Class1.java
+++ service1/src/main/java/com/example/service1/Class1.java
@@ -7,6 +7,7 @@ public class Class1 {
     private String test3 = "test 3";
     private String test4 = "test 4";
     private String test5 = "test 5";
+    private String test6 = "test 6";
 
     public Class1() {
     }
