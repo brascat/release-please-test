ad5b36c08254ecd7d53820cc778e19b5e3a352df
brascat <catarina.bras@diconium.com>
2023-12-12 12:17:08 +0000

    refactor!: service1

--- service1/src/main/java/com/example/service1/Class1.java
+++ service1/src/main/java/com/example/service1/Class1.java
@@ -5,6 +5,7 @@ public class Class1 {
     private String test = "test";
     private String test2 = "test 2";
     private String test3 = "test 3";
+    private String test4 = "test 4";
 
     public Class1() {
     }
