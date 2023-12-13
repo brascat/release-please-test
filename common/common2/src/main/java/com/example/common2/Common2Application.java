c0effc80a6c81eae16050dddc5b1172cff9ff9df
brascat <catarina.bras@diconium.com>
2023-12-13 13:42:13 +0000

    feat: configure gib

---
+++ common/common2/src/main/java/com/example/common2/Common2Application.java
@@ -0,0 +1,13 @@
+package com.example.common2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Common2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Common2Application.class, args);
+    }
+
+}
--- common/src/main/java/com/example/common/File1.java
+++
@@ -1,8 +0,0 @@
-package com.example.common;
-
-public class File1 {
-
-    private int test0 = 0;
-    private int test1 = 1;
-    private int test2 = 2;
-}
