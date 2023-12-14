5f1257501f7eb93cd1a020e4e3c39477cc65edf3
brascat <catarina.bras@diconium.com>
2023-12-14 10:50:12 +0000

    feat(common2): print hello

--- common/common2/src/main/java/com/example/common2/Common2Application.java
+++ common/common2/src/main/java/com/example/common2/Common2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Common2Application {
 
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(Common2Application.class, args);
     }
 
