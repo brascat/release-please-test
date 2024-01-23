232aac41fa6449db76806ecfacedfbe0c1fde031
brascat <catarina.bras@diconium.com>
2024-01-23 17:37:09 +0000

    feat(service2): first commit

--- service2/src/main/java/com/example/service2/Service2.java
+++ service2/src/main/java/com/example/service2/Service2.java
@@ -3,15 +3,6 @@ package com.example.service2;
 public class Service2 {
 
     private String hello;
-    private int test0;
-    private int test1;
-    private int test2;
-    private int test3;
-    private int test4;
-    private int test5;
-    private int test6;
-    private int test7;
-    private int test8;
 
     public Service2(String hello) {
         this.hello = hello;
